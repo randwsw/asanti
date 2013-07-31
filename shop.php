18b60d0b763ec9991ed3fed82620d59fd0dd463a
randwsw <epecoom@gmail.com>
2013-07-31 17:43:00 +0200

    shopfix

--- shop.php
+++ shop.php
@@ -5,9 +5,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Asanti - sklep</title>
 
-	<!-- Include links ---------------------------------------------------------- -->
-	<?php include 'include/links.php'; ?>
-	<!-- ------------------------------------------------------------------------ -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.lavalamp.min.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+
+    
+    <link rel="stylesheet" href="css/shopstyle.css" />
+    <link rel="stylesheet" href="css/sliderstyle.css" />
     
 </head>
 
@@ -27,7 +31,6 @@
 		}
 	?>
         
-<<<<<<< HEAD
     	<div class="sub-menu-container"> 
 		<div class="sub-menu">
 		                
@@ -75,11 +78,6 @@
 				<p class="item-count">0</p>
 			</div>
 		</div>
-=======
-   	<!-- Include submenu -------------------------------------------------------- -->
-	<?php include 'include/submenu.php'; ?>
-	<!-- ------------------------------------------------------------------------ -->
->>>>>>> fcad9aa5cc0702b51efd1e54db58291cc29b114f
 
 		
 	<div class="products">
